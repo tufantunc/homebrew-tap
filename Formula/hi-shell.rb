d35375c43bb6b075543dd72451b0fb1f0168c64c
Tufan Tunç <tufan.tunc.91@gmail.com>
2026-01-26 20:49:05 +0300

    Release v0.1.2

--- Formula/hi-shell.rb
+++ Formula/hi-shell.rb
@@ -1,8 +1,8 @@
 class HiShell < Formula
   desc "An intelligent terminal assistant that translates natural language to shell commands"
   homepage "https://github.com/tufantunc/hi-shell"
-  url "https://github.com/tufantunc/hi-shell/releases/download/v0.1.1/hi-shell-aarch64-apple-darwin.tar.gz"
-  sha256 "5ffe0fba84de406365f930afc939372e42b709f66de1308b724298850f560f19"
+  url "https://github.com/tufantunc/hi-shell/releases/download/v0.1.2/hi-shell-aarch64-apple-darwin.tar.gz"
+  sha256 "a22c4b4d42e58144425e6f27ee7638084ea796100dff031c121599aaf08292cb"
   license "MIT"
 
   def install
